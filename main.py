a362aefd12965ac63c119b6f589caa8d886c2a28
Stapior <33604831+Stapior@users.noreply.github.com>
2019-04-23 18:07:47 +0200

    hhh

--- main.py
+++ main.py
@@ -1,47 +1,83 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 from skimage import data
+from sklearn.neighbors import KNeighborsClassifier
+from scipy import ndimage
 from skimage import measure
-from skimage.exposure import rescale_intensity
-from skimage.filters import sobel, gaussian, rank, threshold_otsu, frangi
+from skimage.exposure import rescale_intensity, equalize_adapthist
+from skimage.feature import blob_log
+from skimage.filters import sobel, gaussian, rank, threshold_otsu, frangi, threshold_adaptive, threshold_local, \
+    threshold_triangle
 from skimage.transform import resize
-from skimage.morphology import disk, opening, square, erosion
+from skimage.morphology import disk, opening, square, erosion, black_tophat, white_tophat, closing, binary_closing, \
+    remove_small_objects, remove_small_holes
 
-images_patch = "resources/images/"
-fig, ax = plt.subplots()
-# for open ppm files
-# im = Image.open(images_patch+"im0001.ppm")
-# im.show()
+imageColor = data.load('C:/Users/Kamil/Documents/GitHub/IwM_eye/resources/Nowy folder/im0255.ppm')
+# imageColor = data.load('C:/Users/Kamil/Documents/GitHub/IwM_eye/resources/images/08_h.JPG')
+# manual = data.load('C:/Users/Kamil/Documents/GitHub/IwM_eye/resources/01_h.tif')
+mask = data.load('C:/Users/Kamil/Documents/GitHub/IwM_eye/resources/Nowy folder/im0255.ah.ppm')
+
+neigh = KNeighborsClassifier(n_neighbors=5)
+imageColor = imageColor[:,:, 1]
 
 
-imageColor = data.load("C:/Users/Piotr/Documents/GitHub/IwM_eye/resources/images/01_dr.JPG")
+y = []
+X = []
+for i in range(10, len(imageColor) - 10):
+    for j in range(10, len(imageColor[0]) - 10):
+        r = []
+        for x in imageColor[i - 5:i + 5][j - 5:j + 5]:
+            r.extend(x)
+        X.append(r)
+        y.append(mask[i][j])
+
+neigh.fit(X, y)
+
+image = data.load('C:/Users/Kamil/Documents/GitHub/IwM_eye/resources/Nowy folder/im0001.ppm')
+res = np.copy(image)
+res = res[:][:][1]
+for i in range(10, len(image) - 10):
+    for j in range(10, len(image) - 10):
+        res[i][j] = neigh.predict(image[i - 5:i + 5][j - 5:j + 5])
+
+plt.imshow(res, cmap="gray")
+plt.show()
+
 imageColor = np.array(imageColor)
 x, y, z = np.shape(imageColor)
-imageColor = resize(imageColor, (int(x / 1.3), int(y / 1.3)))
 
-imageColor = gaussian(imageColor, sigma=5, multichannel=True)
-imageColor[:, :, 2] = 0
-imageColor[:, :, 0] = 0
+# green = imageColor[:, :, 1]
+green = imageColor[:, :, 1] - np.min(imageColor, axis=2)
 
-green = imageColor[:, :, 1]
-# green = rank.median(green, disk(50))
+red = imageColor[:, :, 0]
+red = rescale_intensity(red, in_range=(red.min(), red.max()))
+# green = equalize_adapthist(green)
 
-p1, p99 = np.percentile(green, (1, 99))
-green = rescale_intensity(green, in_range=(p1, p99))
-
-green = frangi(green)
-
-p1, p99 = np.percentile(green, (1, 99))
-green = rescale_intensity(green, in_range=(p1, p99))
-
-# thresh = threshold_otsu(green)
-#
-# green = green > thresh
-green = np.asarray(green)
-p1=0.07
-green[green > p1] = 1
-green[ green <= p1] = 0
-
-ax.imshow(green, cmap="gray")
+plt.imshow(green, cmap="gray")
 plt.show()
-denoised = green
+# green = rescale_intensity(green, in_range=(green.min(), green.max()))
+
+
+# lowpass = ndimage.gaussian_filter(green, 20)
+# green = green + lowpass/2
+
+
+plt.imshow(green, cmap="gray")
+plt.show()
+
+fran = frangi(green, scale_range=(0, 6), scale_step=1)
+
+res1 = fran
+
+plt.imshow(res1, cmap="gray")
+plt.show()
+
+thresh = threshold_triangle(res1)
+res2 = res1 > thresh
+
+res2 = remove_small_objects(res2, 30, 2)
+
+plt.imshow(res2, cmap="gray")
+plt.imsave("new", res2)
+plt.show()
