58974f8e395eaa60db9b62e5bb6cc0006a9b58ee
Paweł Bubak <pawelbubak97@gmail.com>
2019-05-08 21:37:19 +0200

    Dodanie badania statystyk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import scipy
-
+from skimage import io
 import matplotlib.pyplot as plt
 import numpy as np
 from imageio import imsave
@@ -16,11 +16,45 @@ from skimage.morphology import disk, opening, square, erosion, black_tophat, whi
     remove_small_objects, remove_small_holes
 from scipy import signal
 
-imageColor = data.load('C:/Users/Piotr/Documents/GitHub/IwM_eye/resources/images/05_h.jpg')
+
+def statistics(tp, fp, fn, tn):
+    accuracy = (tp + tn) / (tp + fp + fn + tn)
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (fp + tn)
+    return accuracy, sensitivity, specificity
+
+
+def compare_images(img, model):
+    tp, fp, fn, tn = 0, 0, 0, 0
+    for i, row in enumerate(img):
+        for j, item in enumerate(row):
+            model_item = model[i][j]
+            if item == 0 and item == model_item:
+                tn += 1
+            elif item == 0:
+                fn += 1
+            elif item == model_item/255:
+                tp += 1
+            else:
+                fp += 1
+    return tp, fp, fn, tn
+
+
+def load_image(name):
+    img = io.imread('resources/images/' + name + '.jpg')
+    # mask = io.imread('resources/masks/' + name + '.tif')
+    mask = None
+    model = io.imread('resources/model/' + name + '.tif')
+    return img, mask, model
+
+# imageColor = data.load('C:/Users/Piotr/Documents/GitHub/IwM_eye/resources/images/05_h.jpg')
+imageColor = data.load('C:/Users/pawel/Desktop/Projekty/IwM/IwM_eye/resources/images/05_h.jpg')
 # imageColor = data.load('C:/Users/Kamil/Documents/GitHub/IwM_eye/resources/images/08_h.JPG')
-manual = data.load('C:/Users\Piotr/Documents/GitHub/IwM_eye/resources/05_h.tif')
+# manual = data.load('C:/Users\Piotr/Documents/GitHub/IwM_eye/resources/05_h.tif')
+model = data.load('C:/Users/pawel/Desktop/Projekty/IwM/IwM_eye/resources/05_h.tif')
 # mask = data.load('C:/Users/Kamil/Documents/GitHub/IwM_eye/resources/Nowy folder/im0255.ah.ppm')
 
+imageColor, mask, model = load_image('05_h')
 
 imageColor = np.array(imageColor)
 x, y, z = np.shape(imageColor)
@@ -64,21 +98,26 @@ FN = 0
 
 result = np.zeros_like(res2)
 result[res2]=1
-print(manual)
-for i in range(len(manual)):
-    for j in range(len(manual[0])):
+print(model)
+for i in range(len(model)):
+    for j in range(len(model[0])):
         if result[i][j] == 1:
-            if manual[i][j] == result[i][j]:
+            if model[i][j] == result[i][j]:
                 TP += 1
             else:
                 FP += 1
         if result[i][j] == 0:
-            if manual[i][j] == result[i][j]:
+            if model[i][j] == result[i][j]:
                 TN += 1
             else:
                 FN += 1
 
-print(TP, TN, FP, FN)
+tp, fp, fn, tn = compare_images(result, model)
+print("Tp: " + str(tp), "\nFp: " + str(fp), "\nFn: " + str(fn), "\nTn: " + str(tn))
+accuracy, sensitivity, specificity = statistics(tp, fp, fn, tn)
+print("Accuracy: " + str(accuracy), "\nSensitivity: " + str(sensitivity), "\nSpecificity: " + str(specificity))
+
+
 plt.imshow(res2, cmap="gray")
 plt.imsave("new", res2, cmap="gray")
 plt.show()
